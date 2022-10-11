f1eccb58c9f71f21d950a76e0bbb638f2232a15e
Guilherme <guisena_colorado@hotmail.com>
2022-10-10 21:26:57 -0300

    Aula do dia 10/10/2022-- adicionando as activities

--- app/src/main/java/com/example/appflood/MainActivity.java
+++ app/src/main/java/com/example/appflood/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.appflood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -15,6 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonBairro = (Button) findViewById(R.id.buttonBairro);
+        buttonBairro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Bairro.class);
+                startActivityForResult(intent, 22);
+            }
+        });
+
+
+
+
+
     }
 
 }
